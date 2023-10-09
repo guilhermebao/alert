4f9a65b9a57784129408eeeb07de005866a1eba4
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-09 20:39:07 -0300

    feat(domain): Add 'Appointment' entity to represent customer appointments

---
+++ Attendance/Attendance.Domain/Entites/Appointment.cs
@@ -0,0 +1,37 @@
+﻿using Attendance.Domain.Entites;
+using Attendance.Domain.Validation;
+using System;
+
+namespace Attendance.Domain.Entities;
+
+public class Appointment : BaseEntity
+{
+    public DateTime DateTime { get; private set; }
+    public string Message { get; private set; }
+    public Guid CustomerId { get; private set; }
+
+    // A propriedade de navegação para o Cliente (Customer)
+    public Customer Customer { get; private set; }
+
+    public Appointment(Guid id, DateTime dateTime, string message, Guid customerId)
+    {
+        ValidateDomain(dateTime, message, customerId);
+        Id = id;
+    }
+
+    public Appointment(DateTime dateTime, string message, Guid customerId)
+    {
+        ValidateDomain(dateTime, message, customerId);
+    }
+
+    public void ValidateDomain(DateTime dateTime, string message, Guid customerId)
+    {
+        DomainExceptionValidation.When(dateTime < DateTime.Now, "A data e hora do agendamento não podem estar no passado");
+        DomainExceptionValidation.When(string.IsNullOrWhiteSpace(message), "A mensagem do agendamento não pode estar vazia ou em branco");
+        DomainExceptionValidation.When(customerId == Guid.Empty, "O Id do cliente no agendamento não pode ser vazio");
+
+        DateTime = dateTime;
+        Message = message;
+        CustomerId = customerId;
+    }
+}
