da40b2ef25bcfbf393e2fa562e3fbfaa40846941
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-09 23:47:17 -0300

    feat(api, infra.ioc): Configure Swagger to use authorization

--- Attendance/Attendance.Api/Program.cs
+++ Attendance/Attendance.Api/Program.cs
@@ -7,19 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureSwagger();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+ //Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
---
+++ Attendance/Attendance.Infra.Ioc/DependencyInjectionSwagger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Attendance.Infra.Ioc;
+
+public static class DependencyInjectionSwagger
+{
+    public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT de acesso para autenticação de usuário em Attendance. Este token é emitido para conceder acesso seguro a recursos específicos da aplicação e autenticar o usuário com base em sua identidade. Proteja este token e não compartilhe com terceiros"
+            });
+
+            c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{ }
+                }
+            });
+        });
+
+        return services;
+    }
+}
