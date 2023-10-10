f891562c54e8285f34dea2c95cc3cf72a0c7e647
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-10 00:22:48 -0300

    feat(application): Add mappings for Customer and Appointment entities

---
+++ Attendance/Attendance.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Attendance.Application.DTOs;
+using Attendance.Domain.Entities;
+using AutoMapper;
+
+namespace Attendance.Application.Mappings;
+
+public class DomainToDtoMappingProfile : Profile
+{
+    public DomainToDtoMappingProfile()
+    {
+        CreateMap<Customer, CustomerDto>().ReverseMap();
+        CreateMap<Appointment, AppointmentDto>().ReverseMap();
+    }
+}
