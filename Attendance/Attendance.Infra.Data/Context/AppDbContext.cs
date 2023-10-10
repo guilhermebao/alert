d5383f230e8cf23d0515f36c88537175fe8ab505
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-09 21:00:06 -0300

    feat(infra.data): Create AppDbContext with Customer and Appointments DbSets and configure to load configurations from the same assembly

---
+++ Attendance/Attendance.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Attendance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Attendance.Infra.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
