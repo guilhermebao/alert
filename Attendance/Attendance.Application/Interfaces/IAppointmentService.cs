eee8b358fc31c65e830cf692de1f91bbdc64864e
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-10 00:30:52 -0300

    feat(application): Create Appointment interface and implementation

---
+++ Attendance/Attendance.Application/Interfaces/IAppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using Attendance.Application.DTOs;
+
+namespace Attendance.Application.Interfaces;
+
+public interface IAppointmentService
+{
+    Task<IEnumerable<AppointmentDto>> GetAllAppointmentsAsync();
+    Task<AppointmentDto> GetAppointmentByIdAsync(Guid id);
+    Task<AppointmentDto> CreateAppointmentAsync(AppointmentDto appointmentDto);
+    Task<AppointmentDto> UpdateAppointmentAsync(Guid id, AppointmentDto appointmentDto);
+    Task<bool> DeleteAppointmentAsync(Guid id);
+}
---
+++ Attendance/Attendance.Application/Services/AppointmentService.cs
@@ -0,0 +1,64 @@
+﻿using Attendance.Application.DTOs;
+using Attendance.Application.Interfaces;
+using Attendance.Domain.Entities;
+using Attendance.Domain.Interfaces;
+using AutoMapper;
+
+namespace Attendance.Application.Services;
+
+public class AppointmentService : IAppointmentService
+{
+    private readonly IAppointmentRepository _appointmentRepository;
+    private readonly IMapper _mapper;
+
+    public AppointmentService(IAppointmentRepository appointmentRepository, IMapper mapper)
+    {
+        _appointmentRepository = appointmentRepository ?? throw new ArgumentNullException(nameof(appointmentRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task<IEnumerable<AppointmentDto>> GetAllAppointmentsAsync()
+    {
+        var appointments = await _appointmentRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<AppointmentDto>>(appointments);
+    }
+
+    public async Task<AppointmentDto> GetAppointmentByIdAsync(Guid id)
+    {
+        var appointment = await _appointmentRepository.GetByIdAsync(id);
+        return _mapper.Map<AppointmentDto>(appointment);
+    }
+
+    public async Task<AppointmentDto> CreateAppointmentAsync(AppointmentDto appointmentDto)
+    {
+        var appointment = _mapper.Map<Appointment>(appointmentDto);
+        appointment.Id = Guid.NewGuid();
+        var createdAppointment = await _appointmentRepository.AddAsync(appointment);
+        return _mapper.Map<AppointmentDto>(createdAppointment);
+    }
+
+    public async Task<AppointmentDto> UpdateAppointmentAsync(Guid id, AppointmentDto appointmentDto)
+    {
+        var existingAppointment = await _appointmentRepository.GetByIdAsync(id);
+        if (existingAppointment == null)
+        {
+            return null;
+        }
+
+        _mapper.Map(appointmentDto, existingAppointment);
+        var updatedAppointment = _appointmentRepository.Update(existingAppointment);
+        return _mapper.Map<AppointmentDto>(updatedAppointment);
+    }
+
+    public async Task<bool> DeleteAppointmentAsync(Guid id)
+    {
+        var existingAppointment = await _appointmentRepository.GetByIdAsync(id);
+        if (existingAppointment == null)
+        {
+            return false;
+        }
+
+        await _appointmentRepository.RemoveAsync(id);
+        return true;
+    }
+}
