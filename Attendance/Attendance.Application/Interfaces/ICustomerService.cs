5500a33a1a2069e64cba1b915373fd995d323d3e
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-10 00:27:42 -0300

    feat(application): Create Customer interface and implementation

---
+++ Attendance/Attendance.Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using Attendance.Application.DTOs;
+
+namespace Attendance.Application.Interfaces;
+
+public interface ICustomerService
+{
+    Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+    Task<CustomerDto> GetCustomerByIdAsync(Guid id);
+    Task<CustomerDto> CreateCustomerAsync(CustomerDto customerDto);
+    Task<CustomerDto> UpdateCustomerAsync(Guid id, CustomerDto customerDto);
+    Task<bool> DeleteCustomerAsync(Guid id);
+}
---
+++ Attendance/Attendance.Application/Services/CustomerService.cs
@@ -0,0 +1,64 @@
+﻿using Attendance.Application.DTOs;
+using Attendance.Application.Interfaces;
+using Attendance.Domain.Entities;
+using Attendance.Domain.Interfaces;
+using AutoMapper;
+
+namespace Attendance.Application.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ICustomerRepository _customerRepository;
+    private readonly IMapper _mapper;
+
+    public CustomerService(ICustomerRepository customerRepository, IMapper mapper)
+    {
+        _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+    {
+        var customers = await _customerRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<CustomerDto>>(customers);
+    }
+
+    public async Task<CustomerDto> GetCustomerByIdAsync(Guid id)
+    {
+        var customer = await _customerRepository.GetByIdAsync(id);
+        return _mapper.Map<CustomerDto>(customer);
+    }
+
+    public async Task<CustomerDto> CreateCustomerAsync(CustomerDto customerDto)
+    {
+        var customer = _mapper.Map<Customer>(customerDto);
+        customer.Id = Guid.NewGuid();
+        var createdCustomer = await _customerRepository.AddAsync(customer);
+        return _mapper.Map<CustomerDto>(createdCustomer);
+    }
+
+    public async Task<CustomerDto> UpdateCustomerAsync(Guid id, CustomerDto customerDto)
+    {
+        var existingCustomer = await _customerRepository.GetByIdAsync(id);
+        if (existingCustomer == null)
+        {
+            return null;
+        }
+
+        _mapper.Map(customerDto, existingCustomer);
+        var updatedCustomer = _customerRepository.Update(existingCustomer);
+        return _mapper.Map<CustomerDto>(updatedCustomer);
+    }
+
+    public async Task<bool> DeleteCustomerAsync(Guid id)
+    {
+        var existingCustomer = await _customerRepository.GetByIdAsync(id);
+        if (existingCustomer == null)
+        {
+            return false;
+        }
+
+        await _customerRepository.RemoveAsync(id);
+        return true;
+    }
+}
