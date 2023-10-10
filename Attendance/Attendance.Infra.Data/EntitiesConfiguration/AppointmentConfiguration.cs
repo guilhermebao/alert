d5efab1421a21839aac6be57af2c98396f6afe4f
Maximilliam Araujo <maximillian.alex@gmail.com>
2023-10-09 21:30:48 -0300

    feat(infra.data): Add Entity Framework context configurations for Customer and Appointment entities

---
+++ Attendance/Attendance.Infra.Data/EntitiesConfiguration/AppointmentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Attendance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Attendance.Infra.Data.EntitiesConfiguration
+{
+    public class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder.ToTable("Appointments");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.DateTime).IsRequired();
+
+            builder.Property(a => a.Message).HasMaxLength(255);
+
+            builder.HasOne(a => a.Customer).WithMany(c => c.Appointments).HasForeignKey(a => a.CustomerId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Attendance/Attendance.Infra.Data/EntitiesConfiguration/CustomerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Attendance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Attendance.Infra.Data.EntitiesConfiguration;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder) 
+    {
+        builder.ToTable("Customers");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+        builder.Property(c => c.PhoneNumber).HasMaxLength(20).IsRequired();
+        builder.Property(c => c.Address).HasMaxLength(100).IsRequired();
+        builder.Property(c => c.City).HasMaxLength(50).IsRequired();
+        builder.Property(c => c.Neighborhood).HasMaxLength(50).IsRequired();
+        builder.Property(c => c.Number).HasMaxLength(10).IsRequired();
+    }
+}
